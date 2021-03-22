48957ba725bbb70cd9010e42b6c73e6570e27b99
Simran <simran.maurya_cs18@gla.ac.in>
2021-03-22 19:47:00 +0530

    Created the Application with capp package

--- src/main/java/com/mycompany/capp/config/SpringRootConfig.java
+++ src/main/java/com/mycompany/capp/config/SpringRootConfig.java
@@ -1,9 +1,14 @@
 package com.mycompany.capp.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  *
  * @author user
  */
+@Configuration
+@ComponentScan(basePackages={"com.mycompany"})
 public class SpringRootConfig {
     
 }
---
+++ src/main/java/com/mycompany/capp/config/SpringWebConfig.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.capp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ *
+ * @author Simran
+ */
+
+@Configuration
+@ComponentScan(basePackages={"com.mycompany"})
+@EnableWebMvc
+public class SpringWebConfig extends WebMvcConfigurerAdapter{
+    
+    
+}
