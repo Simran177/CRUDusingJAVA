e46703f63d7c9810b446b18623384eb8f881ade0
Simran <simran.maurya_cs18@gla.ac.in>
2021-04-08 20:05:48 +0530

    Data access object created, Create operation out of CRUD performed

--- src/main/java/com/mycompany/capp/config/SpringRootConfig.java
+++ src/main/java/com/mycompany/capp/config/SpringRootConfig.java
@@ -1,5 +1,7 @@
 package com.mycompany.capp.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,7 +10,23 @@ import org.springframework.context.annotation.Configuration;
  * @author user
  */
 @Configuration
-@ComponentScan(basePackages={"com.mycompany"})
+//Spring will identify DAOs from this location
+@ComponentScan(basePackages={"com.mycompany.capp.dao"})
 public class SpringRootConfig {
+    // TODO : Services, DAO, DataSource, Email Sender or some other business
+   @Bean
+    public BasicDataSource getDataSource(){
     
+        BasicDataSource ds= new BasicDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/capp_db");
+        ds.setUsername("root");
+        ds.setPassword("");
+        ds.setMaxTotal(2);
+        ds.setInitialSize(1);
+        ds.setTestOnBorrow(true);
+        ds.setValidationQuery("Select 1"); //connection checking
+        ds.setDefaultAutoCommit(true);
+        return ds;
+    }
 }
---
+++ src/main/java/com/mycompany/capp/dao/ContactDAO.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.capp.dao;
+
+import com.mycompany.capp.domain.Contact;
+import java.util.List;
+
+// ContactDAO will handle CRUD operations for Contact entity
+public interface ContactDAO {
+    
+    public void save(Contact c);
+    public void update(Contact c);
+    public void delete(Contact c);
+    public void delete(Integer contactId);
+    public Contact findById(Integer contactId);
+    public List<Contact> findAll();
+    public List<Contact> findByProperty(String propName, Object propValue);
+}
